83986ffe4d1ff85c6758942b57ff53b5975d9b2e
iryna <irynka.vlasova@gmail.com>
2017-11-28 19:20:09 +0200

    commit

---
+++ src/com.doorshouse/Balance.java
@@ -0,0 +1,12 @@
+package com.doorshouse;
+
+public class Balance {
+   public Balance(String name,int sum,Date date){
+        this.name = name;
+        this.sum = sum;
+        this.date =date;
+    }
+   private String name;
+   private int sum;
+   private Date date;
+}
