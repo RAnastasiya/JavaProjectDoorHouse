d592ae96e720062dc5275a6637816dc7199fbaba
Ladvre <34003528+Ladvre@users.noreply.github.com>
2017-12-01 12:43:28 +0200

    Add files via upload

---
+++ Transport.java
@@ -0,0 +1,10 @@
+package com.doorshouse;
+
+public class Transport{
+	public Transport(String name, int number){
+		this.name = name;
+		this.number = number;
+	}
+	private String name;
+	private int number;
+}
